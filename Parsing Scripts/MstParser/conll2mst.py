b11d9200c6de0bc3581ee0ceb26e0cfc39ba60e7
Iftahp <iftah3@gmail.com>
2014-08-01 11:58:07 +0300

    Python Script
    From conll format to the MSTparser format

---
+++ Parsing Scripts/MstParser/conll2mst.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python
+#See READMEconll2mst file for credits and instructions
+import sys;
+
+# Open Input File for Reading
+infile = open(sys.argv[1],'rt');
+# Open Output File for Writing
+outfile = open(sys.argv[2],'w');
+wrds = ""; pos = ""; labs = ""; par = "";
+
+for line in infile:
+    
+    sent = line.split();
+
+    if len(sent) > 0:
+        wrds += sent[1] + "\t";
+        pos += sent[4] + "\t";
+        labs += sent[7] + "\t";
+        par += sent[6] + "\t";
+    else:
+        outfile.write(wrds); outfile.write('\n'); wrds = "";
+        outfile.write(pos); outfile.write('\n');  pos = "";
+        outfile.write(labs); outfile.write('\n'); labs = "";
+        outfile.write(par); outfile.write('\n');  par = "";
+        outfile.write('\n');
+		
+# Close Input and Output Files
+infile.close();
+outfile.close();
